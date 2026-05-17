e6f0a33290527be86a8bf11c78be1da55cd59fb9
rinkusharma79346-droid <rinkusharma79346@users.noreply.github.com>
2026-05-17 14:02:35 +0000

    chore: bump version to 1.5.1 (WebView thread fix)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.vayu.agenticbrowser"
         minSdk = 26
         targetSdk = 34
-        versionCode = 6
-        versionName = "1.5.0"
+        versionCode = 7
+        versionName = "1.5.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
